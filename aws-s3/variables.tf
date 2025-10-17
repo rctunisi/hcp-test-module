cd22c118764104fc4a11d607e26ece661de31da0
Reginaldo Tunisi <rctunisi@gmail.com>
2025-10-17 13:02:24 -0300

    Update variables.tf

--- aws-s3/variables.tf
+++ aws-s3/variables.tf
@@ -2,3 +2,10 @@ variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
 }
+
+variable "region" {
+  description = "provider region"
+  type        = string
+  default = "us-east-1"
+}
+
